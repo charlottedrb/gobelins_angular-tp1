c9bb0914a32e64c68a404e4c1ef592f393ac2dac
cdWorkshop <corentin.workshop@gmail.com>
2021-10-23 18:47:25 +0200

    x2 just add a ts ignore

--- src/app/pages/home-page/components/switcher/switcher.component.ts
+++ src/app/pages/home-page/components/switcher/switcher.component.ts
@@ -20,28 +20,27 @@ export class SwitcherComponent implements OnInit {
 
   windowScroll() {
     // FIXME
-    /*
+
     if (
       document.body.scrollTop > 100 ||
       document.documentElement.scrollTop > 100
     ) {
+      // @ts-ignore
       document.getElementById("back-to-top").style.display = "inline";
     } else {
+      // @ts-ignore
       document.getElementById("back-to-top").style.display = "none";
-    }*/
+    }
   }
   /**
    * Onclick color change
    * @param theme theme color
    */
   setTheme(theme : string) {
-    // FIXME
-    /*
+    // @ts-ignore
     document
       .getElementById('color-opt')
       .setAttribute('href', 'assets/css/colors/' + theme + '.css');
-
-     */
   }
 
   toggleSwitcher() {
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'home-page',
@@ -8,7 +8,8 @@ import {AfterViewInit, Component } from '@angular/core';
 
 export class HomePageComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   currentSection = 'home-page';
 
@@ -17,33 +18,35 @@ export class HomePageComponent implements AfterViewInit {
     let i = 1;
     setInterval(() => {
       if (i === 1) { // @ts-ignore
-        document.getElementById('text').innerHTML = 'We love make things amazing and simple'; }
-      else if (i === 2) { // @ts-ignore
-        document.getElementById('text').innerHTML = 'Create amazing landing page with Globing'; }
-      else { // @ts-ignore
-        document.getElementById('text').innerHTML = 'Perfect solution for small businesses'; }
-      if (i >= 3) { i = 0; }
+        document.getElementById('text').innerHTML = 'We love make things amazing and simple';
+      } else if (i === 2) { // @ts-ignore
+        document.getElementById('text').innerHTML = 'Create amazing landing page with Globing';
+      } else { // @ts-ignore
+        document.getElementById('text').innerHTML = 'Perfect solution for small businesses';
+      }
+      if (i >= 3) {
+        i = 0;
+      }
       i++;
     }, 2500);
 
 
   }
+
   /**
    * Window scroll method
    */
-  // tslint:disable-next-line: typedef
   windowScroll() {
-    const navbar = document.getElementById('navbar');
-    // FIXME
-    if(!navbar) return;
-
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-      navbar.style.backgroundColor = '#1a1a1a';
-      navbar.style.padding = '15px 0px';
-    }
-    else {
-      navbar.style.backgroundColor = '';
-      navbar.style.padding = '20px';
+      // @ts-ignore
+      document.getElementById('navbar').style.backgroundColor = '#1a1a1a';
+      // @ts-ignore
+      document.getElementById('navbar').style.padding = '15px 0px';
+    } else {
+      // @ts-ignore
+      document.getElementById('navbar').style.backgroundColor = '';
+      // @ts-ignore
+      document.getElementById('navbar').style.padding = '20px';
     }
   }
 
@@ -51,10 +54,8 @@ export class HomePageComponent implements AfterViewInit {
    * Toggle navbar
    */
   toggleMenu() {
-   // FIXME
-    const navbarCollapse = document.getElementById('navbar');
-    if(navbarCollapse) navbarCollapse.classList.toggle('show');
-    //document.getElementById('navbarCollapse').classList.toggle('show');
+    // @ts-ignore
+    document.getElementById('navbarCollapse').classList.toggle('show');
   }
 
   /**
