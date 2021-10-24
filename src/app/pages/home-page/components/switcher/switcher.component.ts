91d74aecf6e66741096fafd5f3021abdd048f080
cdWorkshop <corentin.workshop@gmail.com>
2021-10-24 12:59:09 +0200

    keep & simplify  switcher component

--- src/app/pages/home-page/components/switcher/switcher.component.ts
+++ src/app/pages/home-page/components/switcher/switcher.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'switcher',
@@ -6,21 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./switcher.component.scss']
 })
 
-/**
- * Switcher component
- */
-export class SwitcherComponent implements OnInit {
+export class SwitcherComponent {
 
-  isVisible = false;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   windowScroll() {
-    // FIXME
-
     if (
       document.body.scrollTop > 100 ||
       document.documentElement.scrollTop > 100
@@ -32,19 +21,6 @@ export class SwitcherComponent implements OnInit {
       document.getElementById("back-to-top").style.display = "none";
     }
   }
-  /**
-   * Onclick color change
-   * @param theme theme color
-   */
-  setTheme(theme : string) {
-    // @ts-ignore
-    document
-      .getElementById('color-opt')
-      .setAttribute('href', 'assets/css/colors/' + theme + '.css');
-  }
 
-  toggleSwitcher() {
-    this.isVisible = !this.isVisible;
-  }
 
 }
