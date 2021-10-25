8057e26792c704510a855c12235e049736398ae8
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2021-10-25 14:09:59 +0200

    6.1: mise en place de ngFor dans pricing et création de PricingData

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'pricing',
-  templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.css']
+    selector: 'pricing',
+    templateUrl: './pricing.component.html',
+    styleUrls: ['./pricing.component.css']
 })
 export class PricingComponent implements OnInit {
 
-  constructor() { }
+    pricingData: PricingData[]
 
-  ngOnInit(): void {
-  }
+    constructor() {
+        this.pricingData = [
+            new PricingData('$29', '1GB', '50MB', 'No', 1),
+            new PricingData('$59', '2GB', '500MB', 'Yes', 5),
+            new PricingData('$79', '4GB', '1GB', 'Yes', 10)
+        ]
+    }
 
+    ngOnInit(): void {
+    }
+
+}
+
+class PricingData {
+    price: string
+    bandWidth: string
+    space: string
+    support: string
+    domain: number
+
+    constructor(
+        price: string,
+        bandWidth: string,
+        space: string,
+        support: string,
+        domain: number
+    ) {
+        this.price = price
+        this.bandWidth = bandWidth
+        this.space = space
+        this.support = support
+        this.domain = domain
+    }
 }
