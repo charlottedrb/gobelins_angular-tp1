61ca4014882c8c67845c806c3fb92a063cb158fe
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2021-10-25 14:52:26 +0200

    6.4: ngFor et ngClass dans Service Component

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'services',
-  templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+    selector: 'services',
+    templateUrl: './services.component.html',
+    styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+    serviceData: ServiceData[]
 
-  ngOnInit(): void {
-  }
+    constructor() {
+        this.serviceData = [
+            new ServiceData("Digital Design", 'ti-settings'),
+            new ServiceData("Unlimited Colors", 'ti-palette'),
+            new ServiceData("Strategy Solutions", 'ti-stats-up'),
+            new ServiceData("Awesome Support", 'ti-package'),
+            new ServiceData("Truly Multipurpose", 'ti-dashboard'),
+            new ServiceData("Easy to customize", 'ti-headphone')
+        ]
+    }
 
+    ngOnInit(): void {
+    }
+
+}
+
+class ServiceData {
+    title: string
+    icon: string
+
+    constructor(title: string, icon: string) {
+        this.title = title
+        this.icon = icon
+    }
 }
