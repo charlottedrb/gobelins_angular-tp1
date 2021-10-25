be9db8f18bf6742ac0d8c50823c7de88f377c358
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2021-10-25 13:19:32 +0200

    4.3: création des autres components (contact, faq, features, pricing, services, cta, team

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -8,13 +8,27 @@ import {ClientComponent} from './client/client.component';
 import {SwitcherComponent} from './switcher/switcher.component';
 import {ScrollspyDirective} from './scrollspy.directive';
 import {FooterComponent} from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { FaqComponent } from './faq/faq.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ServicesComponent } from './services/services.component';
+import { CtaComponent } from './cta/cta.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
     declarations: [
         ClientComponent,
         SwitcherComponent,
         ScrollspyDirective,
-        FooterComponent
+        FooterComponent,
+        ContactComponent,
+        FaqComponent,
+        FeaturesComponent,
+        PricingComponent,
+        ServicesComponent,
+        CtaComponent,
+        TeamComponent
     ],
     imports: [
         CommonModule,
@@ -26,6 +40,13 @@ import {FooterComponent} from './footer/footer.component';
         SwitcherComponent,
         ScrollspyDirective,
         FooterComponent,
+        ContactComponent,
+        FaqComponent,
+        FeaturesComponent,
+        PricingComponent,
+        ServicesComponent,
+        CtaComponent,
+        TeamComponent
     ]
 })
 export class SharedModule {
