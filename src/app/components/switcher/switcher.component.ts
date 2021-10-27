569754e6c7a07b2a240f6a1b1c3138468e68f7dd
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2021-10-27 09:25:34 +0200

    8 : correction ts-lint

--- src/app/components/switcher/switcher.component.ts
+++ src/app/components/switcher/switcher.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
     selector: 'switcher',
@@ -9,38 +10,58 @@ import {Component} from '@angular/core';
 export class SwitcherComponent {
     // autre méthode pour le toggle
     // isVisible: boolean
-    themes = [
-        'default',
-        'pink',
-        'yellow',
-        'green',
-        'purple',
-        'light-blue'
+
+    // en utilisant un objet Theme, on évite les erreurs d'ordre des thèmes pour l'HTML (class='color1'...)
+    themes: Theme[] = [
+        new Theme(1, 'default'),
+        new Theme(2, 'pink'),
+        new Theme(3, 'yellow'),
+        new Theme(4, 'green'),
+        new Theme(5, 'purple'),
+        new Theme(6, 'light-blue'),
     ]
 
+    constructor(@Inject(DOCUMENT) private readonly document: Document) {}
+
     public windowScroll(): void {
+        const html = document.getElementById("back-to-top")
+        if(!html) return
+
         if (
             document.body.scrollTop > 100 ||
             document.documentElement.scrollTop > 100
         ) {
             // @ts-ignore
-            document.getElementById("back-to-top").style.display = "inline";
+            html.style.display = "inline";
         } else {
             // @ts-ignore
-            document.getElementById("back-to-top").style.display = "none";
+            html.style.display = "none";
         }
     }
 
     public toggleSettings(): void {
-        // @ts-ignore
-        document.querySelector('#style-switcher').classList.toggle('active')
+        const html = document.querySelector('#style-switcher')
+        if(!html) return
+
+        html.classList.toggle('active')
 
         // autre méthode
         // this.isVisible = !this.isVisible
     }
 
     public setTheme(theme: string): void {
-        // @ts-ignore
-        document.getElementById('color-opt').setAttribute('href', 'assets/css/colors/' + theme + '.css')
+        const html = document.getElementById('color-opt')
+        if(!html) return
+        html.setAttribute('href', 'assets/css/colors/' + theme + '.css')
+    }
+}
+
+class Theme {
+    colorId: number
+    name: string
+
+    constructor(colorId: number, name: string) {
+        this.colorId = colorId
+        this.name = name
     }
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
-import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
-  selector: 'home-page', //sélecteur html pour appeler le compo
-  templateUrl: './home-page.component.html',// template associé
-  styleUrls: ['./home-page.component.scss']// style associé pour surcharger les styles principaux
+    selector: 'home-page', //sélecteur html pour appeler le compo
+    templateUrl: './home-page.component.html',// template associé
+    styleUrls: ['./home-page.component.scss']// style associé pour surcharger les styles principaux
 
 })
 
@@ -11,72 +12,73 @@ import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 // OnInit, AfterViewInit et OnDestroy sont des hooks. On doit implémenter les méthodes correspondantes (ngOnInit...)
 export class HomePageComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  // variables et functions convention de nommage camelCase
-  currentSection = 'home-page';
+    // variables et functions convention de nommage camelCase
+    currentSection = 'home-page';
 
-  constructor() {
-    // exécuté en premier !
+    constructor(@Inject(DOCUMENT) private readonly document: Document) {
+        // exécuté en premier !
 
-    // exemple de log à checker dans la console du navigateur
-    console.log('(exemple de log) section courante: ' + this.currentSection); // utilisation de this pour appels à fonctions, variables, ...
-  }
-
-  ngOnInit() {
-    // exécuté automatiquement à l'initialisation du composant
-    // obligatoire si implements OnInit (qui est facultatif)
-    // D'autres existent, voir https://angular.io/guide/lifecycle-hooks
-
-  }
-
-  ngAfterViewInit() {
-    // exécuté automatiquement après l'initialisation de la vue
-    // obligatoire si implements AfterViewInit (qui est facultatif)
-    // D'autres existent, voir https://angular.io/guide/lifecycle-hooks
-  }
-
-  // Toujours utiliser public ou private pour les fonctions, variables , constantes ...
-  // Si pas précisé, c'est public par défaut mais on s'expose à des soucis de lint
-  // NOTE : Pour les appels depuis le html, mettre en public
-  public windowScroll(): void {
-    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-      // @ts-ignore
-      document.getElementById('navbar').style.backgroundColor = '#1a1a1a';
-      // @ts-ignore
-      document.getElementById('navbar').style.padding = '15px 0px';
-    } else {
-      // @ts-ignore
-      document.getElementById('navbar').style.backgroundColor = '';
-      // @ts-ignore
-      document.getElementById('navbar').style.padding = '20px';
+        // exemple de log à checker dans la console du navigateur
+        console.log('(exemple de log) section courante: ' + this.currentSection); // utilisation de this pour appels à fonctions, variables, ...
     }
-  }
 
- // Utiliser void si la fonction ne renvoie rien
-  private returnNothing (): void {
-    const fake = this.returnABoolean(false)
-  }
+    ngOnInit() {
+        // exécuté automatiquement à l'initialisation du composant
+        // obligatoire si implements OnInit (qui est facultatif)
+        // D'autres existent, voir https://angular.io/guide/lifecycle-hooks
 
-  // Préciser le type ( boolean, any, unknown ...)
-  private returnABoolean (input : boolean): boolean {
-    const fake = !input;
-    return fake;
-  }
+    }
 
-  public toggleMenu(): void {
-    // @ts-ignore
-    document.getElementById('navbarCollapse').classList.toggle('show');
-  }
+    ngAfterViewInit() {
+        // exécuté automatiquement après l'initialisation de la vue
+        // obligatoire si implements AfterViewInit (qui est facultatif)
+        // D'autres existent, voir https://angular.io/guide/lifecycle-hooks
+    }
+
+    // Toujours utiliser public ou private pour les fonctions, variables , constantes ...
+    // Si pas précisé, c'est public par défaut mais on s'expose à des soucis de lint
+    // NOTE : Pour les appels depuis le html, mettre en public
+    public windowScroll(): void {
+        const html = document.getElementById('navbar')
+        if(!html) return
+
+        if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+            html.style.backgroundColor = '#1a1a1a';
+            html.style.padding = '15px 0px';
+        } else {
+            html.style.backgroundColor = '';
+            html.style.padding = '20px';
+        }
+    }
+
+    // Utiliser void si la fonction ne renvoie rien
+    private returnNothing(): void {
+        const fake = this.returnABoolean(false)
+    }
+
+    // Préciser le type ( boolean, any, unknown ...)
+    private returnABoolean(input: boolean): boolean {
+        const fake = !input;
+        return fake;
+    }
+
+    public toggleMenu(): void {
+        const html = document.getElementById('navbarCollapse')
+        if(!html) return
+
+        html.classList.toggle('show');
+    }
 
 
-  public onSectionChange(sectionId: string): void {
-    this.currentSection = sectionId;
-  }
+    public onSectionChange(sectionId: string): void {
+        this.currentSection = sectionId;
+    }
 
-  ngOnDestroy() {
-    // exécuté automatiquement avant la desctruction du composant
-    // obligatoire si implements OnDestroy (qui est facultatif)
-    // Important : Si on a souscrit à des abonnements, se désabonner ici pour éviter les fuites de mémoires
-    // D'autres existent, voir https://angular.io/guide/lifecycle-hooks
-  }
+    ngOnDestroy() {
+        // exécuté automatiquement avant la desctruction du composant
+        // obligatoire si implements OnDestroy (qui est facultatif)
+        // Important : Si on a souscrit à des abonnements, se désabonner ici pour éviter les fuites de mémoires
+        // D'autres existent, voir https://angular.io/guide/lifecycle-hooks
+    }
 
 }
